91d4edde574157c5a65afde84fad77150c9ad85e
Jacob Zhang <jacob-z@live.de>
2019-11-05 23:18:26 +0100

    Add license headers

--- Models/CodeGeneration/CSharp/CSharpTester.cs
+++ Models/CodeGeneration/CSharp/CSharpTester.cs
@@ -1,12 +1,26 @@
-﻿using System;
+﻿// Thesis - An Excel to code converter
+// Copyright (C) 2019 Jacob Zhang
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
-using Microsoft.CodeAnalysis.CSharp.Scripting;
 using Microsoft.CodeAnalysis.Scripting;
 using Microsoft.CSharp.RuntimeBinder;
-using Thesis.Models.VertexTypes;
 using Thesis.ViewModels;
 
 namespace Thesis.Models.CodeGeneration.CSharp
--- Views/MainWindowEvents.cs
+++ Views/MainWindowEvents.cs
@@ -1,7 +1,22 @@
-﻿using System;
+﻿// Thesis - An Excel to code converter
+// Copyright (C) 2019 Jacob Zhang
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+using System;
 using System.ComponentModel;
 using System.Linq;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -10,10 +25,8 @@ using Syncfusion.UI.Xaml.CellGrid.Helpers;
 using Syncfusion.UI.Xaml.Diagram;
 using Syncfusion.UI.Xaml.Spreadsheet.Helpers;
 using Syncfusion.XlsIO.Implementation;
-using Thesis.Models;
 using Thesis.Models.VertexTypes;
 using Thesis.ViewModels;
-using GroupCollection = Syncfusion.UI.Xaml.Diagram.GroupCollection;
 
 namespace Thesis.Views
 {
